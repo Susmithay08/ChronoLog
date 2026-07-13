2d7d467c989a36496fcca5a90262c3928e1349b7
Susmitha Yeddula <yeddulasusmitha@gmail.com>
2026-07-13 01:42:42 -0500

    changed main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
 from app.api import logs
 from app.core.database import init_db
 
@@ -11,3 +13,6 @@ app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], all
 def startup(): init_db()
 
 app.include_router(logs.router, prefix="/api")
+
+frontend_dist = Path(__file__).resolve().parent.parent.parent / "frontend" / "dist"
+app.mount("/", StaticFiles(directory=frontend_dist, html=True), name="static")
